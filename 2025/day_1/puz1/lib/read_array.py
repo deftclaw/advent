e00d360788cde61d60ca4b41ce9d20b4d41b5516
deftclaw <deftclaw@briskbear.rocks>
2026-03-04 15:52:21 -0700

    Explain the purpose of the List Comprehension

--- 2025/day_1/puz1/lib/read_array.py
+++ 2025/day_1/puz1/lib/read_array.py
@@ -4,4 +4,4 @@ def read_lines(leaf):
     with open(leaf, 'r') as handle:
         lines = handle.read().split('\n')
 
-    return [ x for x in lines if x.strip() ]
+    return [ x for x in lines if x.strip() ]  # Remove empty lines from the array
